aa5088ddc6f38241e886062dab7e536632d3e75b
ronnahom96 <ronnahom96@gmail.com>
2019-02-19 16:22:16 +0200

    add firebase to the project

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, "9.0"
+use_frameworks!
+
+target 'MyProject' do
+ pod 'Firebase'
+end
---
+++ Pods/Target Support Files/Pods-iWareTests/Pods-iWareTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iWareTests : NSObject
+@end
+@implementation PodsDummy_Pods_iWareTests
+@end
